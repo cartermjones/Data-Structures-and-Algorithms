84a55345ee2996fc7687ba1da418a42521ec0a94
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-09-26 13:42:11 -0500

    Create main.c

---
+++ Stack/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+struct Stack
+{
+    int top;
+    unsigned size;
+    int* array;
+};
+
+struct Stack* createStack(unsigned size)
+{
+    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack));
+    stack->size = size;
+    stack->top = -1;
+    stack->array = (int*) malloc(stack->size * sizeof(int));
+    return stack;
+};
+
+int isFull(struct Stack* stack)
+{
+    return stack->top == stack->size - 1;
+}
+
+int isEmpty(struct Stack* stack)
+{
+    return stack->top == -1;
+}
+
+void push(struct Stack* stack, int item)
+{
+    if(isFull(stack))
+        return;
+    stack->array[++stack->top] = item;
+    printf("%d pushed to stack\n", item);
+}
+
+int pop(struct Stack* stack)
+{
+    if(isEmpty(stack))
+        return INT_MIN;
+    return stack->array[stack->top--];
+}
+
+
+int main()
+{
+    struct Stack* stack = createStack(10);
+
+    push(stack, 10);
+    push(stack, 20);
+    push(stack, 30);
+
+    printf("%d popped from stack\n", pop(stack));
+
+    return 0;
+}
